dfa163e548a4176e011eee4dc0eb3162ab3c8c93
Júnior Medeiros <98979459+Medeiros000@users.noreply.github.com>
2026-09-25 18:46:03 -0300

    add registration page and fix import path for RegisterPage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import ConfigPage from '@/pages/Config';
 import ErrorBoundary from "@/components/ErrorBoundary";
 import HomePage from "@/pages/Home";
 import NewsPage from "@/pages/NewsPage";
-import RegisterPage from "@/pages/RegistePage";
+import RegisterPage from "@/pages/RegisterPage";
 import LoginPage from "./components/LoginForm";
 import { useUserStore, useNavigationStore } from "@/stores";
 
